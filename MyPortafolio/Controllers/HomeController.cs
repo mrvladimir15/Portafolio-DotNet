c547bf3626ae356b408cf08da178783bfe049344
Vladimir González <gonzalezdotel@gmail.com>
2023-08-24 11:38:44 -0400

    Adding projects information in a separate class

--- MyPortafolio/Controllers/HomeController.cs
+++ MyPortafolio/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyPortafolio.Models;
+using MyPortafolio.Services;
 using System.Diagnostics;
 using System.Net.Cache;
 
@@ -16,42 +17,10 @@ namespace MyPortafolio.Controllers
 
         public IActionResult Index()
         {
-            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var RepositorioProyectos = new RepositorioProyectos();
+            var proyectos = RepositorioProyectos.ObtenerProyectos().Take(3).ToList();
             var modelo = new HomeIndexDTO() { Proyectos = proyectos };
             return View(modelo);
-        }
-
-        private List<ProyectoDTO> ObtenerProyectos()
-        {
-            return new List<ProyectoDTO>() {new ProyectoDTO
-            {
-                Titulo = "Amazon",
-                Descripcion="E-Commerce en ASP.NET Core",
-                Link = "https://amazon.com",
-                ImagenURL = "/img/amazon.png"
-            },
-            new ProyectoDTO
-            {
-                Titulo = "New York Times",
-                Descripcion="Página de noticias en React",
-                Link = "https://nytimes.com",
-                ImagenURL = "/img/nyt.png"
-            },
-            new ProyectoDTO
-            {
-                Titulo = "Reddit",
-                Descripcion="Red social para compartir en comunidades",
-                Link = "https://reddit.com",
-                ImagenURL = "/img/reddit.png"
-            },
-            new ProyectoDTO
-            {
-                Titulo = "Steam",
-                Descripcion="Tienda virtual para la compra de videojuegos",
-                Link = "https://store.steampowered",
-                ImagenURL = "/img/steam.png"
-            },
-            };
         }
 
         public IActionResult Privacy()
---
+++ MyPortafolio/Services/RepositorioProyectos.cs
@@ -0,0 +1,40 @@
+﻿using MyPortafolio.Models;
+
+namespace MyPortafolio.Services
+{
+    public class RepositorioProyectos
+    {
+        public List<ProyectoDTO> ObtenerProyectos()
+        {
+            return new List<ProyectoDTO>() {new ProyectoDTO
+            {
+                Titulo = "Amazon",
+                Descripcion="E-Commerce en ASP.NET Core",
+                Link = "https://amazon.com",
+                ImagenURL = "/img/amazon.png"
+            },
+            new ProyectoDTO
+            {
+                Titulo = "New York Times",
+                Descripcion="Página de noticias en React",
+                Link = "https://nytimes.com",
+                ImagenURL = "/img/nyt.png"
+            },
+            new ProyectoDTO
+            {
+                Titulo = "Reddit",
+                Descripcion="Red social para compartir en comunidades",
+                Link = "https://reddit.com",
+                ImagenURL = "/img/reddit.png"
+            },
+            new ProyectoDTO
+            {
+                Titulo = "Steam",
+                Descripcion="Tienda virtual para la compra de videojuegos",
+                Link = "https://store.steampowered",
+                ImagenURL = "/img/steam.png"
+            },
+            };
+        }
+    }
+}
